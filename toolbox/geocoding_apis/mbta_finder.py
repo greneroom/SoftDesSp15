91d9ff3efb1ce737af462f47c2d125986269c4c3
David <david.abrahams@students.olin.edu>
2015-03-04 10:44:27 -0500

    got dict from json url, and got lat long from place name

--- toolbox/geocoding_apis/mbta_finder.py
+++ toolbox/geocoding_apis/mbta_finder.py
@@ -6,6 +6,8 @@ Find the MBTA stops closest to a given location.
 Full instructions are at:
 https://sites.google.com/site/sd15spring/home/project-toolbox/geocoding-and-web-apis
 """
+from pprint import pprint
+import re
 
 import urllib   # urlencode function
 import urllib2  # urlopen function (better than urllib version)
@@ -17,6 +19,8 @@ GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
 MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 
+FENWAY = "https://maps.googleapis.com/maps/api/geocode/json?address=Fenway%20Park"
+
 
 # A little bit of scaffolding if you want to use it
 
@@ -25,6 +29,15 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
+
+    f = urllib2.urlopen(url)  # file object
+    response_text = f.read()  # str
+    response_data = json.loads(response_text)  #dict
+
+    assert isinstance(response_data, dict)
+    return response_data
+
+def get_geocode_url(place_name):
     pass
 
 
@@ -36,7 +49,24 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
-    pass
+
+    place_name = re.sub(r"\s+", '+', place_name)  #convert whitespace to '+'
+
+    g_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    g_url += place_name
+
+    json = get_json(g_url)
+
+    result = json["results"][0]
+    geo_dict = result["geometry"]
+    loc_dict = geo_dict["location"]
+
+    lat = loc_dict["lat"]
+    long = loc_dict["lng"]
+
+    return lat, long
+
+
 
 
 def get_nearest_station(latitude, longitude):
@@ -57,3 +87,6 @@ def find_stop_near(place_name):
     """
     pass
 
+
+if __name__ == '__main__':
+    pprint(get_lat_long('Shoreline Amphitheatre, Mountain View'))
